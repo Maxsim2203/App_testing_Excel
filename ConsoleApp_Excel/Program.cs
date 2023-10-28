cdb58dd46b211464c2a34f9bdce4fe171955137b
NRR <pianzovmaxsim236@gmail.com>
2023-10-28 11:03:07 +0300

    fix

--- ConsoleApp_Excel/Program.cs
+++ ConsoleApp_Excel/Program.cs
@@ -9,6 +9,7 @@ using DocumentFormat.OpenXml.Spreadsheet;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Data;
 using static ClosedXML.Excel.XLWorkbook;
+using DocumentFormat.OpenXml.Drawing.Charts;
 
 internal class Program
 {
@@ -57,90 +58,55 @@ internal class Program
             }
 
             Console.CursorTop = down;
-            
-                if (y == top)
+            string? path_file;
+            if (y == top)
+            {
+                Console.Write("Введите полный путь к файлу: ");
+                string? path_file = Console.ReadLine();
+                bool exist = File.Exists(path_file);
+                if (exist == true)
                 {
-                    //Console.Write("");
-                    Console.Write("Введите полный путь к файлу: ");
-                    string? path_file = Console.ReadLine();
-                    bool exist = File.Exists(path_file);
-                    if (exist == true)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Файл существует.");
-                    }
+                    Console.Clear();
+                    Console.WriteLine("Файл существует.");
+                }
                 if (exist == false)
-                   
                 {
-
                     Console.Clear();
                     Console.WriteLine("Путь к файлу указан неверно!");
                 }
-                else if (y == top + 1)
+            }
+            else if (y == top + 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Укажите наименование товара: ");
+                string? nam_tov = Console.ReadLine();
+                XLWorkbook workbook;
+                using (workbook = new XLWorkbook(path_file))
                 {
-                    if (exist == true)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Укажите наименование товара: ");
-                        string? nam_tov = Console.ReadLine();
-                        XLWorkbook workbook;
-                        using (workbook = new XLWorkbook(path_file))
-                        {
-                            var worksheet = workbook.Worksheets.First();
+                    var worksheet = workbook.Worksheets.First();
 
-                            var column = worksheet.Column("C");
+                    var column = worksheet.Column("B");
 
-                            var columnCells = column.CellsUsed();
+                    var columnCells = column.CellsUsed();
 
-                            var cell = columnCells.First(c => c.Value.ToString().Contains(nam_tov));
+                    var cell = columnCells.First(c => c.Value.ToString().Contains(nam_tov));
 
-                            var row = cell.WorksheetRow();
+                    var row = cell.WorksheetRow();
 
-                            Console.WriteLine(row.Cell("A").Value);
-                            Console.WriteLine(row.Cell("B").Value);
-                            Console.WriteLine(row.Cell("D").Value);
-
-
-                        }
-                    }
-                    if (exist == false)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Товар не выбран!");
-                    }
-                    Console.Clear();
-
-                }
-                else if (y == top + 2)
-                {
-                    //string pathToFile = "D:\\1.xlsx";
-                    //////Открываем книгу.                                                                                                                                                        
-                    //Microsoft.Office.Interop.Excel.Workbook ObjWorkBook = ObjExcel.Workbooks.Open(pathToFile, 0, false, 5, "", "", false, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
-                    //////Выбираем таблицу(лист).
-                    //Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
-                    //ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook;
-
-                    //// Указываем номер столбца (таблицы Excel) из которого будут считываться данные.
-                    //int numCol = 4;
-
-                    //Range usedColumn = ObjWorkSheet.Column[numCol];
-                    //System.Array myvalues = (System.Array)usedColumn.Cells.Value2;
-                    //string[] strArray = myvalues.OfType<object>().Select(o => o.ToString()).ToArray();
-
-                    //////Выходим из программы Excel.
-                    //ObjExcel.Quit();
-
-                    ////Создание экземпляра Workbook
-                    //Workbook workbook = new Workbook();
-
-                    ////Получение первой рабочей страницы
-                }
-                else if (y == top + 3)
-                {
-                    Console.WriteLine("четыре");
+                    Console.WriteLine(row.Cell("A").Value);
+                    Console.WriteLine(row.Cell("B").Value);
+                    Console.WriteLine(row.Cell("D").Value);
                 }
             }
-                 
+            else if (y == top + 2)
+            {
+                Console.WriteLine("три");
+            }
+            else if (y == top + 3)
+            {
+                Console.WriteLine("четыре");
+            }
+            
             Console.WriteLine("ENTER - продолжение работы");
             Console.WriteLine("ESC - выход");
             key = Console.ReadKey().Key;
@@ -148,6 +114,7 @@ internal class Program
             Console.Clear();
             if (key == ConsoleKey.Escape)
             {
+               Console.WriteLine(y);
                Console.WriteLine("ППрограмма завершила работу. До свидания."); //Два П т.к по нажатию клавиши ESC убирается первый символ 
                break;
             }
@@ -166,12 +133,36 @@ internal class Program
 //Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
 //ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Товары;
 
-//// Указываем номер столбца (таблицы Excel) из которого будут считываться данные.
-//int numCol = 2;
+    //// Указываем номер столбца (таблицы Excel) из которого будут считываться данные.
+    //int numCol = 2;
 
-//Range usedColumn = ObjWorkSheet.UsedRange.Columns[numCol];
+    //Range usedColumn = ObjWorkSheet.UsedRange.Columns[numCol];
+    //System.Array myvalues = (System.Array)usedColumn.Cells.Value2;
+    //string[] strArray = myvalues.OfType<object>().Select(o => o.ToString()).ToArray();
+
+    //// Выходим из программы Excel.
+    //ObjExcel.Quit();
+
+//else if (y == top + 2)
+//{
+                    //string pathToFile = "D:\\1.xlsx";
+                    //////Открываем книгу.                                                                                                                                                        
+                    //Microsoft.Office.Interop.Excel.Workbook ObjWorkBook = ObjExcel.Workbooks.Open(pathToFile, 0, false, 5, "", "", false, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+                    //////Выбираем таблицу(лист).
+                    //Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
+                    //ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook;
+
+//// Указываем номер столбца (таблицы Excel) из которого будут считываться данные.
+//int numCol = 4;
+
+//Range usedColumn = ObjWorkSheet.Column[numCol];
 //System.Array myvalues = (System.Array)usedColumn.Cells.Value2;
 //string[] strArray = myvalues.OfType<object>().Select(o => o.ToString()).ToArray();
 
-//// Выходим из программы Excel.
+//////Выходим из программы Excel.
 //ObjExcel.Quit();
+
+////Создание экземпляра Workbook
+//Workbook workbook = new Workbook();
+
+////Получение первой рабочей страницы
