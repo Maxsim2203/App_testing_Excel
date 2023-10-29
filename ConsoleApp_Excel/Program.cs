e3018fdd337a26898fba5ce3e7ca8057202bf795
NRR <pianzovmaxsim236@gmail.com>
2023-10-29 20:58:03 +0300

    update fixed main

--- ConsoleApp_Excel/Program.cs
+++ ConsoleApp_Excel/Program.cs
@@ -27,7 +27,9 @@ internal class Program
         string? naim_cli = " ";
         string? cost_tov = " ";
         string? date_zak = " ";
-        string? FIO_cli = " "; 
+        string? FIO_cli = " ";
+        //var Cell_new = "";
+
         bool check_1 = false; //Контроль наличия товара
         bool check_2 = false; //Контроль наличия заказа
         while (ind == true)
@@ -39,7 +41,7 @@ internal class Program
 
             Console.WriteLine("1. Указать путь к файлу с данными");
             Console.WriteLine("2. Указать наименование товара и вывести список клентов, заказавших товар");
-            Console.WriteLine("3. Изменить контактное лицо клиена по критериям");
+            Console.WriteLine("3. Изменить контактное лицо клиента");
             Console.WriteLine("4. Определить золотого клиента");
 
             int down = Console.CursorTop;
@@ -124,9 +126,9 @@ internal class Program
                         if (row_3.Cell("B").Value.ToString() == kod_tov)
                         {
                             check_2 = true;
-                            kod_cli = row_3.Cell("C").Value.ToString();  //287 - Чай
-                            kol_tov = row_3.Cell("E").Value.ToString();  //5 - для первого заказа 10 - для второго
-                            date_zak = row_3.Cell("F").Value.ToString(); //14.03.2023 - для первого заказа 22.06.2023 - для второго
+                            kod_cli = row_3.Cell("C").Value.ToString();  
+                            kol_tov = row_3.Cell("E").Value.ToString();  
+                            date_zak = row_3.Cell("F").Value.ToString(); 
 
                             // Находим на 2 листе ячейку с кодом клиента и  запоминаем наименование клиента
                             var worksheet_2 = workbook.Worksheet(2);
@@ -180,28 +182,22 @@ internal class Program
                     {
                         if (row.Cell("B").Value.ToString() == nai_cli)
                         {
+                            check_2 = true;
                            naim_cli = row.Cell("B").Value.ToString();
                            FIO_cli = row.Cell("D").Value.ToString();
+                           //var Cell_new = row.Cell("D");
                            Console.WriteLine(FIO_cli);
+                           //Console.WriteLine(Cell_new);
+                           Console.WriteLine("Укажите ФИО нового контактного лица");
+                           string? FIO_new_cli = Console.ReadLine();
+                           row.Cell("D").Value = FIO_new_cli;
+                           workbook.Save();
                         }
-                       else
-                       {
-                           // Console.WriteLine(FIO_cli);
-                       }
-                        //foreach (var row_2 in worksheet_2.Rows(firstRow_2.RowNumber(), lastRow_2.RowNumber()))
-                        //{
-                        //    if (row_2.Cell("D").Value.ToString() == FIO_cli)
-                        //    {
-                        //        naim_cli = row_2.Cell("B").Value.ToString();
-                        //        Console.WriteLine(naim_cli); 
-                        //    }
-                        //}
-                        //Console.WriteLine(naim_cli);
                     }
-                    Console.WriteLine(FIO_cli);
-                    
-                    //Console.WriteLine(naim_cli, " " + FIO_cli);            
-                    //naim_cli
+                    if (check_2 == false)
+                    {
+                        Console.WriteLine("Не верно");
+                    }
                 }
                 Console.WriteLine("Изменения занесены в таблицу");
             }
@@ -233,110 +229,3 @@ internal class Program
         }
     }
 }
-
-
-
-//Console.WriteLine(kod_cli);
-//Console.WriteLine(naim_cli);
-//Console.WriteLine(kol_tov);
-//Console.WriteLine(date_zak);
-//key = Console.ReadKey().Key;
-
-
-//string pathToFile = @"D:\data.xlsx";
-////Console.WriteLine("Hello, World!");
-////Создаём приложение.
-//Microsoft.Office.Interop.Excel.Application ObjExcel = new Microsoft.Office.Interop.Excel.Application();
-////Открываем книгу.                                                                                                                                                        
-//Microsoft.Office.Interop.Excel.Workbook ObjWorkBook = ObjExcel.Workbooks.Open(pathToFile, 0, false, 5, "", "", false, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
-////Выбираем таблицу(лист).
-//Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
-//ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook.Товары;
-
-//// Указываем номер столбца (таблицы Excel) из которого будут считываться данные.
-//int numCol = 2;
-
-//Range usedColumn = ObjWorkSheet.UsedRange.Columns[numCol];
-//System.Array myvalues = (System.Array)usedColumn.Cells.Value2;
-//string[] strArray = myvalues.OfType<object>().Select(o => o.ToString()).ToArray();
-
-//// Выходим из программы Excel.
-//ObjExcel.Quit();
-
-//else if (y == top + 2)
-//{
-//string pathToFile = "D:\\1.xlsx";
-//////Открываем книгу.                                                                                                                                                        
-//Microsoft.Office.Interop.Excel.Workbook ObjWorkBook = ObjExcel.Workbooks.Open(pathToFile, 0, false, 5, "", "", false, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
-//////Выбираем таблицу(лист).
-//Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
-//ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ObjWorkBook;
-
-//// Указываем номер столбца (таблицы Excel) из которого будут считываться данные.
-//int numCol = 4;
-
-//Range usedColumn = ObjWorkSheet.Column[numCol];
-//System.Array myvalues = (System.Array)usedColumn.Cells.Value2;
-//string[] strArray = myvalues.OfType<object>().Select(o => o.ToString()).ToArray();
-
-//////Выходим из программы Excel.
-//ObjExcel.Quit();
-
-////Создание экземпляра Workbook
-//Workbook workbook = new Workbook();
-
-////Получение первой рабочей страницы
-///                            //Console.Write(row.Cell("A").Value + " ");
-//Console.Write(row.Cell("B").Value + " ");
-//Console.WriteLine(row.Cell("D").Value);
-//Console.WriteLine(cell);
-
-// Определение столбца, в котором будет производиться поиск
-//var column = worksheet.Column("B");
-//var columnCells = column.CellsUsed();
-//var cell = columnCells.FirstOrDefault();
-
-/*foreach (var row in worksheet.Rows(firstRow.RowNumber(), lastRow.RowNumber()))
-{
-    if (row.Cell("B").Value.ToString() == nam_tov)
-    {
-        Console.Write(row.Cell("A").Value + " ");
-        Console.Write(row.Cell("B").Value + " ");
-        Console.WriteLine(row.Cell("D").Value);
-        //Console.WriteLine(cell);
-    }
-
-}
-
-    /*var cell = columnCells.F;
-    // Получение значения ячейки
-    //var cell = columnCells.First(c => c.Value.ToString().Contains(nam_tov));
-    //var cell = columnCells.(1);
-
-
-    Console.Write(cellValue);
-    Console.WriteLine(nam_tov);
-
-    var currentCell = columnCells.Cast<Cell>().FirstOrDefault();
-    // Обработка каждой строки
-    //foreach (var cell in row.CellsUsed())
-
-
-    foreach (var row in worksheet.Rows(firstRow.RowNumber(), lastRow.RowNumber()))
-    {
-        // Получение значения ячейки
-        //var cell = columnCells.First(c => c.Value.ToString().Contains(nam_tov));
-        //var cell = columnCells.(1);
-
-        var cellValue = currentCell.Value.ToString();
-        Console.Write(cellValue);
-        Console.WriteLine(nam_tov);
-
-        if (cellValue != nam_tov)
-        {
-            Console.Write(row.Cell("A").Value + " ");
-            Console.Write(row.Cell("B").Value + " ");
-            Console.WriteLine(row.Cell("D").Value);*/
-
-
-
